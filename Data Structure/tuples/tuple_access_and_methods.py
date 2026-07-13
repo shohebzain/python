9169ce16e2b60fe932784ac3134d3223fc69eea0
Mohammad Shoheb <shohebzain745@gmail.com>
2026-07-13 22:57:23 +0530

    Update tuple_access_and_methods.py

--- Data Structure/tuples/tuple_access_and_methods.py
+++ Data Structure/tuples/tuple_access_and_methods.py
@@ -6,7 +6,7 @@ print(my_tuple[1:4]) # (2, 3, 4)
 
 #two methods for tuples
 #1. count() - returns the number of times a specified value appears in the tuple
-my_tuple = (1, 2, 3, 4, 5, 2, 2)
+my_tuple = (1, 2, 3, 4, 5, 2, 2) 
 print(my_tuple.count(2)) # 3
 #2. index() - returns the index of the first occurrence of a specified value
 print(my_tuple.index(2)) # 1
