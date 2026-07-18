60a5688c6b610f9b71e3e951c45a0461598d1a2c
Mohammad Shoheb <shohebzain745@gmail.com>
2026-07-18 23:13:27 +0530

    Add Guess Number game implementation

---
+++ Data Structure/Guess_number.py
@@ -0,0 +1,24 @@
+import random
+
+def play_game():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+    
+    print("Welcome to the Higher-Lower Game!")
+    print("I'm thinking of a number between 1 and 100.")
+
+    while True:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            
+            if guess < secret_number:
+                print("Too low! Try a higher number.")
+            else:
+                print(f"Congratulations! You guessed the number in {attempts} attempts.")
+                break
+        except ValueError:
+            print("Please enter a valid integer.")
+
+if __name__ == "__main__":
+    play_game()
