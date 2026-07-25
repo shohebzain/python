e43167c8599e22e058093afd1b996569d98b2688
Mohammad Shoheb <shohebzain745@gmail.com>
2026-07-25 23:56:01 +0530

    Fix indentation and formatting in constructor.py

--- OOPS/constuctor.py
+++ OOPS/constuctor.py
@@ -4,7 +4,7 @@ class Factory:
         self.zips = zips
         self.pockets = pockets
 
-reebok = Factory("leather",2,3)
+reebok = Factory("leather",2,3) 
 campus = Factory("nylon",3,3)
 print(reebok.pockets) #3
 print(reebok) #<__main__.Factory object at 0x0000021D59F37230>
