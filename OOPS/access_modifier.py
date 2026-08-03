2f36fc745581cd4694ac12e5aa53c6cbbd555980
Mohammad Shoheb <shohebzain745@gmail.com>
2026-08-03 22:05:30 +0530

    Update access_modifier.py

--- OOPS/access_modifier.py
+++ OOPS/access_modifier.py
@@ -2,7 +2,7 @@ class Factory:
     a = "Hyderabad"
 
     def show(self):
-        print("this factory in hyderabad")
+        print("this factory in hyderabad") 
     
 class Knr(Factory):
     def show2(self):
